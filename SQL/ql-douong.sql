f80f027d63badbf42de1205f42b5541dd4ff508b
DZT711 <nguyensihuynsh711@gmail.com>
2025-09-15 10:30:39 +0700

    connected from Java to MySQL using JAR

---
+++ SQL/ql-douong.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th9 15, 2025 lúc 05:28 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `ql-douong`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chitiethoadon`
+--
+-- Error reading structure for table ql-douong.chitiethoadon: #1932 - Table &#039;ql-douong.chitiethoadon&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.chitiethoadon: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`chitiethoadon`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dogiadung`
+--
+-- Error reading structure for table ql-douong.dogiadung: #1932 - Table &#039;ql-douong.dogiadung&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.dogiadung: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`dogiadung`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `douong`
+--
+-- Error reading structure for table ql-douong.douong: #1932 - Table &#039;ql-douong.douong&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.douong: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`douong`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoadon`
+--
+-- Error reading structure for table ql-douong.hoadon: #1932 - Table &#039;ql-douong.hoadon&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.hoadon: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`hoadon`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+-- Error reading structure for table ql-douong.khachhang: #1932 - Table &#039;ql-douong.khachhang&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.khachhang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`khachhang`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhanvien`
+--
+-- Error reading structure for table ql-douong.nhanvien: #1932 - Table &#039;ql-douong.nhanvien&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.nhanvien: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`nhanvien`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+-- Error reading structure for table ql-douong.sanpham: #1932 - Table &#039;ql-douong.sanpham&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.sanpham: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`sanpham`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoan`
+--
+-- Error reading structure for table ql-douong.taikhoan: #1932 - Table &#039;ql-douong.taikhoan&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.taikhoan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`taikhoan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoanlienket`
+--
+-- Error reading structure for table ql-douong.taikhoanlienket: #1932 - Table &#039;ql-douong.taikhoanlienket&#039; doesn&#039;t exist in engine
+-- Error reading data for table ql-douong.taikhoanlienket: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ql-douong`.`taikhoanlienket`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
