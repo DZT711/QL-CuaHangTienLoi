903f58ee7264dfbdf02d8434ec9f8bf4a760aa0b
Tuan Truong <101103234+tvantuan911@users.noreply.github.com>
2025-09-23 16:27:37 +0700

    feat: added method to input custome information

--- dto/KhachHangDTO.java
+++ dto/KhachHangDTO.java
@@ -1,11 +1,16 @@
 package dto;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class KhachHangDTO {
     private String maKH;
     private String ho;
     private String ten;
     private String gioiTinh;
-    private Date ngaySinh;
+    private LocalDate ngaySinh;
     private String diaChi;
     private String dienThoai;
     // private String trangThai;
@@ -13,7 +18,7 @@ public class KhachHangDTO {
 
     public KhachHangDTO() {}
 
-    public KhachHangDTO(String maKH, String ho, String ten, String gioiTinh, Date ngaySinh, String diaChi, String dienThoai) {
+    public KhachHangDTO(String maKH, String ho, String ten, String gioiTinh, LocalDate ngaySinh, String diaChi, String dienThoai) {
         this.maKH = maKH;
         this.ho = ho;
         this.ten = ten;
@@ -55,11 +60,11 @@ public class KhachHangDTO {
         this.gioiTinh = gioiTinh;
     }
 
-    public Date getNgaySinh() {
+    public LocalDate getNgaySinh() {
         return ngaySinh;
     }
 
-    public void setNgaySinh(Date ngaySinh) {
+    public void setNgaySinh(LocalDate ngaySinh) {
         this.ngaySinh = ngaySinh;
     }
 
@@ -78,4 +83,67 @@ public class KhachHangDTO {
     public void setDienThoai(String dienThoai) {
         this.dienThoai = dienThoai;
     }   
+
+    public void nhapThongTinKhachHang() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập họ khách hàng: ");
+        this.ho = scanner.nextLine().trim();
+        while (this.ho.isEmpty()) {
+            System.out.println("Họ khách hàng không được để trống, vui lòng nhập lại.");
+            System.out.print("Nhập họ khách hàng: ");
+            this.ho = scanner.nextLine().trim();
+        }
+
+        System.out.print("Nhập tên khách hàng: ");
+        this.ten = scanner.nextLine().trim();
+        while (this.ten.isEmpty()) {
+            System.out.println("Tên khách hàng không được để trống, vui lòng nhập lại.");
+            System.out.print("Nhập tên khách hàng: ");
+            this.ten = scanner.nextLine().trim();
+        }
+
+        System.out.print("Nhập giới tính khách hàng (Nam/Nữ): ");
+        this.gioiTinh = scanner.nextLine().trim();
+        while (!this.gioiTinh.equalsIgnoreCase("Nam") && !this.gioiTinh.equalsIgnoreCase("Nữ")) {
+            System.out.println("Giới tính không hợp lệ, vui lòng nhập lại (Nam/Nữ): ");
+            this.gioiTinh = scanner.nextLine().trim();
+        }
+
+        while (true) {
+            try {
+                System.out.print("Nhập ngày sinh khách hàng (dd/mm/yyyy): ");
+                String ngaySinh = scanner.nextLine();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                this.ngaySinh = LocalDate.parse(ngaySinh, formatter);
+
+                if (this.ngaySinh.isAfter(LocalDate.now())) {
+                    System.out.println("Ngày sinh không được lớn hơn ngày hiện tại!");
+                    continue;
+                }
+                break;
+            } catch (DateTimeParseException e) {
+                System.out.println("Sai định dạng! Vui lòng nhập lại (dd/mm/yyyy).");
+            }
+        }
+
+        System.out.print("Nhập địa chỉ khách hàng: ");
+        this.diaChi = scanner.nextLine();
+
+        while (this.diaChi.isEmpty()) {
+            System.out.println("Địa chỉ khách hàng không được để trống, vui lòng nhập lại.");
+            System.out.print("Nhập địa chỉ khách hàng: ");
+            this.diaChi = scanner.nextLine().trim();
+        }
+
+        System.out.print("Nhập điện thoại khách hàng (10 số): ");
+        this.dienThoai = scanner.nextLine();
+        while (true) {
+            this.dienThoai = scanner.nextLine().trim();
+            if (this.dienThoai.matches("\\d{10}")) {
+                break;
+            } else {
+                System.out.print("Số điện thoại không hợp lệ, vui lòng nhập lại (10 số): ");
+            }
+        }
+    }
 }
