79c193e1c1f6ecfb5eccb8d564ae6a3978f95e29
Tuan Truong <101103234+tvantuan911@users.noreply.github.com>
2025-09-28 14:36:16 +0700

    upload file TaiKhoanDTO.java

---
+++ dto/TaiKhoanDTO.java
@@ -0,0 +1,41 @@
+package dto;
+
+public class TaiKhoanDTO {
+    private String username;
+    private String password;
+    // private String maNV;
+    private String role;
+
+    public TaiKhoanDTO() {}
+
+    public TaiKhoanDTO(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+}
