2fd00b930ece1013ee439e7c17185334eed3ce53
Tuan Truong <101103234+tvantuan911@users.noreply.github.com>
2025-10-06 16:26:17 +0700

    added find invoice by ID

--- src/dao/HoaDonDAO.java
+++ src/dao/HoaDonDAO.java
@@ -3,12 +3,13 @@ package dao;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDateTime;
 import dto.HoaDonDTO;
 import util.JDBCUtil;
 
 public class HoaDonDAO {
-    public static List<HoaDonDTO> getAllHoaDon() {
-        String query = "SELECT MaHD, MaKH, MaNV, MaKM, TongTien, NgayLapHD FROM HOADON";
+    /*public static List<HoaDonDTO> getAllHoaDon() {
+        String query = "SELECT MaHD, MaKH, MaNV, TongTien, NgayLapHD FROM HOADON";
         List<HoaDonDTO> list = new ArrayList<>();
 
         try (Connection conn = JDBCUtil.getConnection()) {
@@ -19,29 +20,30 @@ public class HoaDonDAO {
                     rs.getString("MaHD"), 
                     rs.getString("MaKH"), 
                     rs.getString("MaNV"), 
-                    rs.getString("MaKM"), 
                     rs.getInt("TongTien"), 
-                    rs.getDate("NgayLapHD"))
+                    rs.getTimestamp("NgayLapHD").toLocalDateTime())
                 );
             }
         } catch (SQLException e) {
             System.err.println("Lỗi khi lấy tất cả hóa đơn: " + e.getMessage());
         }
         return list;
-    }
+    }*/
 
     public static void themHoaDon(HoaDonDTO hd) {
-        String query = "INSERT INTO HOADON (MaHD, MaKH, MaNV, MaKM, TongTien, NgayLapHD) VALUES (?, ?, ?, ?, ?, current_timestamp());";
+        String query = "INSERT INTO HOADON (MaHD, MaKH, MaNV, TongTien, PhuongThucTT, TienKhachDua, TienThua) VALUES (?, ?, ?, ?, ?, ?, ?);";
 
         try (Connection conn = JDBCUtil.getConnection()) {
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, hd.getMaHD());
             stmt.setString(2, hd.getMaKH());
             stmt.setString(3, hd.getMaNV());
-            stmt.setString(4, hd.getMaKM());
-            stmt.setInt(5, hd.getTongTien());
-            stmt.setDate(6, new java.sql.Date(hd.getNgayLapHD().getTime()));
-            int rowAffected = stmt.executeUpdate();
+            stmt.setInt(4, hd.getTongTien());
+            stmt.setString(5, hd.getPhuongThucTT());
+            stmt.setInt(6, hd.getTienKhachDua());
+            stmt.setInt(7, hd.getTienThua());
+
+            int rowAffected = stmt.executeUpdate(); 
             if (rowAffected > 0) {
                 System.out.println("Thêm hóa đơn thành công");
             } else {
@@ -89,5 +91,31 @@ public class HoaDonDAO {
             System.err.println("Lỗi khi tạo mã hóa đơn: " + e.getMessage());
         }
         return newID;
+    }
+
+    public static HoaDonDTO timHoaDon(String maHD) {
+        String query = "SELECT MaKH, MaNV, TienKhachDua, TienThua, TongTien, PhuongThucTT, NgayLapHD FROM HOADON WHERE MaHD = ?";
+
+        try (Connection conn = JDBCUtil.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, maHD);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new HoaDonDTO(
+                    maHD,
+                    rs.getString("MaKH"),
+                    rs.getString("MaNV"),
+                    rs.getInt("TienKhachDua"),
+                    rs.getInt("TienThua"),
+                    rs.getInt("TongTien"),
+                    rs.getTimestamp("NgayLapHD").toLocalDateTime(),
+                    rs.getString("PhuongThucTT")
+                );
+            }
+        } catch (SQLException e) {
+            System.err.println("Lỗi khi in hóa đơn: " + e.getMessage());
+        }
+        return null;
     }
 }
