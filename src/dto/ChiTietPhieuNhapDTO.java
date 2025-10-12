73864ddfe8e5d92e8279687fb7e2be8c15af253f
Tuan Truong <101103234+tvantuan911@users.noreply.github.com>
2025-10-12 22:05:49 +0700

    update properties of ChiTietPhieuNhapDTO

--- src/dto/ChiTietPhieuNhapDTO.java
+++ src/dto/ChiTietPhieuNhapDTO.java
@@ -1,16 +1,19 @@
 package dto;
 
 public class ChiTietPhieuNhapDTO {
-    private String maPhieu, maSP;
-    private int soLuong, giaNhap;
+    private String maPhieu, maSP, tenSP, donViTinh;
+    private int soLuong, giaNhap, thanhTien;
 
     public ChiTietPhieuNhapDTO() {}
     
-    public ChiTietPhieuNhapDTO(String maPhieu, String maSP, int soLuong, int giaNhap) {
+    public ChiTietPhieuNhapDTO(String maPhieu, String maSP, String tenSP, String donViTinh, int soLuong, int giaNhap, int thanhTien ) {
         this.maPhieu = maPhieu;
         this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.donViTinh = donViTinh;
         this.soLuong = soLuong;
         this.giaNhap = giaNhap;
+        this.thanhTien = thanhTien;
     }
     
     public String getMaPhieu() {
@@ -29,6 +32,22 @@ public class ChiTietPhieuNhapDTO {
         this.maSP = maSP;
     }
 
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+
     public int getSoLuong() {
         return soLuong;
     }
@@ -43,5 +62,13 @@ public class ChiTietPhieuNhapDTO {
 
     public void setGiaNhap(int giaNhap) {
         this.giaNhap = giaNhap;
+    }
+
+    public int getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThanhTien(int thanhTien) {
+        this.thanhTien = thanhTien;
     }
 }
